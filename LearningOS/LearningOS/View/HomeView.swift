0ffc3a63d7d8f0f3ec0247b038bc4afdb9e82be2
swapnil gupta <swapnilgupta.xi@gmail.com>
2024-12-29 19:36:16 +0530

    initial-cards

---
+++ LearningOS/LearningOS/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  LearningOS
+//
+//  Created by Abundent on 28/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
